4c3d790923cd601241538247cdf99eee9b247ca0
Viraj Solanki <virazssolanki@gmail.com>
2019-06-04 08:53:21 +0530

    bgcolor textarea

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class AddMenuForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ['menu', 'price']
-
-		def __init__(self, *args, **kwargs):
-			super(ProductForm, self).__init__(*args, **kwargs)
-			self.fields['description'].widget.attrs={ 'id': 'autocomplete'}
+	def __init__(self, *args, **kwargs):
+		super(AddMenuForm, self).__init__(*args, **kwargs) # Call to ModelForm constructor
+		self.fields['menu'].widget.attrs['cols'] = 5
+		self.fields['menu'].widget.attrs['rows'] = 5
--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,21 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
+PRICE_CHOICES = (
+    ('40','40'),
+    ('45', '45'),
+    ('50','50'),
+    ('55','55'),
+    ('60','60'),
+    ('70','70'),
+    ('80','80'),
+)
+
 class Post(models.Model):
 	menu = models.TextField(max_length=150)
 	date_posted = models.DateTimeField(default=timezone.now)
-	price = models.CharField(max_length=3, default='40')
+	price = models.CharField(max_length=3,choices=PRICE_CHOICES, default='40')
 	active = models.BooleanField(default=True)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
