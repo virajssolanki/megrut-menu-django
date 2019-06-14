cd57995785b84cc61b494bea39c3fe42faedb4f2
Viraj Solanki <virazssolanki@gmail.com>
2019-06-14 16:05:52 +0530

    order form lengh

--- order/forms.py
+++ order/forms.py
@@ -3,7 +3,7 @@ from .models import Number
 
 
 class AddNumberForm(forms.ModelForm):
-	
+	number = forms.CharField(max_length=10, min_length=10)
 	class Meta:
 		model = Number
 		fields = ['number']
--- order/models.py
+++ order/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Number(models.Model):
 	date_posted = models.DateTimeField(default=timezone.now)
-	number = models.CharField(max_length=10, blank=True)
+	number = models.CharField(max_length=10, default='7046717674')
 
 	def __str__(self):
 		return self.number
