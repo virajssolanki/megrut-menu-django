7ae877ea8a0fb95229b9a4d2ea844fe303d44942
Viraj Solanki <virazssolanki@gmail.com>
2019-06-20 23:35:44 +0530

    order count me in info

--- order/forms.py
+++ order/forms.py
@@ -3,7 +3,7 @@ from .models import Number
 
 
 class AddNumberForm(forms.ModelForm):
-	number = forms.CharField(max_length=10, min_length=10)
+	mobile_number = forms.CharField(max_length=10, min_length=10)
 	class Meta:
 		model = Number
-		fields = ['number']
+		fields = ['mobile_number']
