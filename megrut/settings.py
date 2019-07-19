b80d99e77710c4d3cb5af6c4e2b9401833119547
Viraj Solanki <virazssolanki@gmail.com>
2019-07-19 22:47:18 +0530

    logout redirect to login

--- megrut/settings.py
+++ megrut/settings.py
@@ -137,7 +137,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-LOGOUT_REDIRECT_URL = 'website'
+LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
